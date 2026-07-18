c1719f4d223adee982d5bcbbe95ae70e40360c7d
mahdim43 <hooshmandm43@gmail.com>
2026-07-18 18:41:43 +0330

    Change site URL to mahdim43.github.io root

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: "https://mahdim43.github.io",
-  base: "/surendar-portfolio",
+  base: "/",
 });
